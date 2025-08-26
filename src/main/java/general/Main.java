5cb7217593bfffc89ce8491efff68d0889529525
KarelStehlik <karel.stehlik.cz@gmail.com>
2025-08-26 21:42:09 +0200

    removed whatever this bs is

--- src/main/java/general/Main.java
+++ src/main/java/general/Main.java
@@ -11,7 +11,7 @@ public final class Main {
   }
 
   public static void main(String[] args) {
-    System.loadLibrary("renderdoc");
+    //System.loadLibrary("renderdoc");
     Log.write(Version.getVersion());
     Audio.init();
     var win = Window.get();
