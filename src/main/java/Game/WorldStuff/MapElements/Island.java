64042fb81dd4d79cc3c5789e999bceb4724dfa0a
GloriousRatEmperor <franta.stehlik.cz@gmail.com>
2026-06-19 17:39:46 +0200

    ...
    dunno no work

--- src/main/java/Game/WorldStuff/MapElements/Island.java
+++ src/main/java/Game/WorldStuff/MapElements/Island.java
@@ -1,4 +1,76 @@
 package Game.WorldStuff.MapElements;
 
+import Game.WorldStuff.Game;
+import Game.WorldStuff.TdWorld;
+import Game.WorldStuff.WorldParameters;
+import GlobalUse.Constants;
+import GlobalUse.Data;
+import windowStuff.Button;
+import windowStuff.ButtonArray;
+import windowStuff.GraphicsOnly.Sprite.Sprite;
+import windowStuff.GraphicsOnly.Sprite.SpriteBatching;
+
 public class Island {
+    private ButtonArray levels;
+    private int[] position;
+
+    public Island(int[] pos, int count) {
+        this.position=pos;
+        pos[0]+=600;
+        levels=makeLevels();
+    }
+
+    public void activate(SpriteBatching bs) {
+        levels.addAllToBs(bs);
+        Game.get().addMouseDetect(levels);
+        levels.show();
+    }
+
+    private ButtonArray makeLevels() {
+        int mapCount = Data.listMaps().length;
+        Button[] buttons = new Button[mapCount];
+        for (int i = 0; i < mapCount; i++) {
+            buttons[i] = makeMapButton(i);
+        }
+        return new ButtonArray(2,
+                buttons,
+                new Sprite("Button", 4), 75, position[0], position[1], 10,
+                1, 1);
+    }
+
+    private Button makeMapButton(int id) {
+
+        String mapName = Data.listMaps()[id];
+        Sprite sp = new Sprite(mapName, 6).setSize(10, 10);
+        Button b = new Button(Game.get().getSpriteBatching("main"), sp, (x, y) -> {
+            Game.get().getWorld().delete();
+            TdWorld level = new TdWorld(generateWorldParams(id));
+            Game.get().setWorld(level);
+        });
+        return b;
+    }
+
+    //the pre random default for the next map - updated every map.
+    //map gets overwritten
+    private WorldParameters defaultParams = new WorldParameters(0, 3, 0, 1);
+
+    private WorldParameters generateWorldParams(int id) {
+        WorldParameters worldParameters = new WorldParameters(id, defaultParams.maxRound, defaultParams.startDifficulty, defaultParams.roundScaling);
+        //TODO apply random modifiers and stuff, add stuff to world parameters for stuff like cash starve, max monkeys, list of favored monkeys, list of bonuses that carry over from map to map etc
+        if (defaultParams.maxRound < 10)
+            defaultParams.maxRound++;
+        defaultParams.roundScaling += 0.1f;
+        return worldParameters;
+    }
+
+
+    public void show() {
+        levels.show();
+    }
+
+    //not the opposite of activate, I'm assuming deactivation is done through the world dying
+    public void hide() {
+        levels.hide();
+    }
+
 }
--- src/main/java/Game/WorldStuff/MapElements/MapSelect.java
+++ src/main/java/Game/WorldStuff/MapElements/MapSelect.java
@@ -6,63 +6,48 @@ import Game.WorldStuff.World;
 import Game.WorldStuff.WorldParameters;
 import GlobalUse.Constants;
 import GlobalUse.Data;
+import org.joml.Vector2f;
 import windowStuff.Button;
 import windowStuff.ButtonArray;
 import windowStuff.GraphicsOnly.Sprite.Sprite;
 import windowStuff.GraphicsOnly.Sprite.SpriteBatching;
 
-public class MapSelect {
-    private SpriteBatching bs;
-    ButtonArray islands;
+import java.util.ArrayList;
+import java.util.Vector;
 
+public class MapSelect {
+    ArrayList<Island> islands;
 
     public MapSelect(){
-        int mapCount = Data.listMaps().length;
-        Button[] buttons = new Button[mapCount];
-        for (int i = 0; i < mapCount; i++) {
-            buttons[i] = makeMapButton(i);
-        }
-        islands = new ButtonArray(2,
-                buttons,
-                new Sprite("Button", 4), 75, Constants.screenSize.x, Constants.screenSize.y, 10,
-                1, 1);
-    }
-    private Button makeMapButton(int id) {
-        String mapName = Data.listMaps()[id];
-        Sprite sp = new Sprite(mapName, 6).setSize(10, 10);
-        Button b = new Button(Game.get().getSpriteBatching("main"), sp, (x, y) -> {
-            Game.get().getWorld().delete();
-            TdWorld level=new TdWorld(generateWorldParams(id));
-            Game.get().setWorld(level);
-        });
-        return b;
+        islands.add(makeIsland());
+        islands.add(makeIsland());
+        islands.add(makeIsland());
+        islands.add(makeIsland());
+
     }
 
-    //the pre random default for the next map - updated every map.
-    //map gets overwritten
-    private WorldParameters defaultParams=new WorldParameters(0,3,0,1);
-
-    private WorldParameters generateWorldParams(int id){
-        WorldParameters worldParameters=new WorldParameters(id,defaultParams.maxRound,defaultParams.startDifficulty,defaultParams.roundScaling);
-        //TODO apply random modifiers and stuff, add stuff to world parameters for stuff like cash starve, max monkeys, list of favored monkeys, list of bonuses that carry over from map to map etc
-        if(defaultParams.maxRound<10)
-            defaultParams.maxRound++;
-        defaultParams.roundScaling+=0.1f;
-        return worldParameters;
+    int[] lastIslandPos={200,(int)(Constants.screenSize.y*0.65)};
+    private Island makeIsland(){
+        return new Island(lastIslandPos,islands.size());
     }
+
     //deactivation is done through the world dying, activate once per world instance
     public void activate(){
-        bs = Game.get().getSpriteBatching("main");
-        islands.addAllToBs(bs);
-        Game.get().addMouseDetect(islands);
-        islands.show();
+        SpriteBatching bs = Game.get().getSpriteBatching("main");
+        for(Island isl:islands) {
+            isl.activate(bs);
+        }
     }
 
     public void show(){
-        islands.show();
+        for(Island isl:islands) {
+            isl.show();
+        }
     }
     //not the opposite of activate, I'm assuming deactivation is done through the world dying
     public void hide(){
-        islands.hide();
+        for(Island isl:islands) {
+            isl.hide();
+        }
     }
 }
